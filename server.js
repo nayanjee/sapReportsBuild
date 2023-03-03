15722b088b68ded0157fb8a1fac287b0363e01e1
unknown <Nayan.Prakash@LARENON.NET>
2023-03-03 14:52:36 +0530

    updates

--- server.js
+++ server.js
@@ -4,21 +4,28 @@ const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
 const app = express();
+
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-extended: false
+    extended: false
 }));
+
 // Point static path to dist
 app.use(express.static(path.join(__dirname, '/')));
+
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
 res.sendFile(path.join(__dirname, '/index.html'));
 });
-/ Get port from environment and store in Express. /
+
+// Get port from environment and store in Express.
 const port = process.env.PORT || '7874';
+
 app.set('port', port);
-/ Create HTTP server. /
+
+// Create HTTP server.
 const server = http.createServer(app);
-/ Listen on provided port, on all network interfaces. /
+
+// Listen on provided port, on all network interfaces.
 server.listen(port, () =>  console.log(`App is running on localhost: ${port}`))
